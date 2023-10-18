8d068c543018db6903dee2074615feb64a384194
zaza <dontaskme0a@yandex.ru>
2023-10-18 23:12:57 +0900

    Выполнение второго задания

--- recipes/recipes/urls.py
+++ recipes/recipes/urls.py
@@ -11,7 +11,7 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))123123
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))123123123123
 """
 
 from django.urls import path
