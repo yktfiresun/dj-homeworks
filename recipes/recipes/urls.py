58aecf4e9f296ff2c542977c75d605167467c6bc
zaza <dontaskme0a@yandex.ru>
2023-10-26 20:29:54 +0900

    Выполнение второго задания

--- recipes/recipes/urls.py
+++ recipes/recipes/urls.py
@@ -12,6 +12,9 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', in123213clude('blog.urls'))123123123123
+
+
+
 """
 
 from django.urls import path
