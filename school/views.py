684710565b1f0892491dfc5bb7ff8625244c29b9
yktfiresun <dontaskme0a@yandex.ru>
2023-10-30 14:51:42 +0900

    Задание миграции

---
+++ school/views.py
@@ -0,0 +1,17 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    ordering = 'group'
+    context = {'object_list': Student.objects.all().order_by(ordering)}
+
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+
+
+
+    return render(request, template, context)
